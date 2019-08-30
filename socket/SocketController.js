dd935c5049878d44aa760ac67a8cf8beb5861353
Joshua Sherman <jsherman@valcom.com>
2019-08-30 14:18:42 -0400

    finishes get all tours sorted

--- socket/SocketController.js
+++ socket/SocketController.js
@@ -1,7 +1,7 @@
 const debug = require('debug')('WebJamSocket:SocketController');
 const bookController = require('../model/book/book-controller');
 const tourController = require('../model/tour/tour-controller');
-const tourData = require('../tour.json');
+const tourData = require('../tour');
 
 const { tour } = tourData;
 class SocketController {
@@ -19,9 +19,7 @@ class SocketController {
         debug('deleted');
         await this.bookController.createDocs({ title: 'first book', type: 'test' });
       });
-      this.tourController.deleteAllDocs().then(() => {
-        this.tourController.createDocs(tour);
-      });
+      this.tourController.deleteAllDocs().then(() => { this.tourController.createDocs(tour); });
     }
     // In here we handle our incoming realtime connections and listen for events.
     this.scServer.on('connection', (socket) => {
@@ -32,9 +30,7 @@ class SocketController {
         this.scServer.exchange.publish('sample', this.count);
       });
       const interval = setInterval(() => {
-        socket.emit('random', {
-          number: Math.floor(Math.random() * 5),
-        });
+        socket.emit('random', { number: Math.floor(Math.random() * 5) });
       }, 1000);
       socket.on('disconnect', () => {
         this.count -= 1;
@@ -44,8 +40,10 @@ class SocketController {
       });
       socket.on('getTours', async () => {
         let allTours;
-        try { allTours = await this.tourController.getAll(); } catch (e) { debug(e.message); throw e; }
-        socket.emit('allTours', allTours);
+        try { allTours = await this.tourController.getAllSort({ datetime: -1 }); } catch (e) {
+          debug(e.message); return socket.emit('error', `${e.message}`);
+        }
+        return socket.emit('allTours', allTours);
       });
     });
     return Promise.resolve(true);
--- tests/socket/SocketController.test.js
+++ tests/socket/SocketController.test.js
@@ -18,6 +18,16 @@ describe('SocketController', () => {
     const socketController = new SocketController(sServer);
     socketController.bookController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
     socketController.tourController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
+    socketController.tourController.getAllSort = jest.fn(() => Promise.resolve(true));
+    const result = await socketController.routing();
+    jest.advanceTimersByTime(2000);
+    expect(result).toBe(true);
+  });
+  it('runs routingbut has error on getting the tours', async () => {
+    const socketController = new SocketController(sServer);
+    socketController.bookController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
+    socketController.tourController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
+    socketController.tourController.getAllSort = jest.fn(() => Promise.reject(new Error('bad')));
     const result = await socketController.routing();
     jest.advanceTimersByTime(2000);
     expect(result).toBe(true);
