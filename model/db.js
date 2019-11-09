235957d75b1ab6257bdc27e02039261e5ba1a454
Joshua Sherman <jsherman@valcom.com>
2019-11-09 10:10:12 -0500

    fixes mongoose warning

--- model/db.js
+++ model/db.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;// use es6 default promise library in mongoose
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
-const mongoOptions = { keepAlive: 1, useNewUrlParser: true, autoIndex: true };
+const mongoOptions = {
+  keepAlive: 1, useNewUrlParser: true, autoIndex: true, useUnifiedTopology: true, 
+};
 /* istanbul ignore if */
 if (process.env.NODE_ENV === 'production') mongoOptions.autoIndex = false;
 let uri = process.env.MONGO_DB_URI;
