67090ee1123e7a9f414f65d53f03313b90127aee
Joshua Sherman <jsherman@valcom.com>
2019-11-19 08:14:49 -0500

    unit tests and refactoring

--- tests/socket/SocketController.test.js
+++ tests/socket/SocketController.test.js
@@ -22,15 +22,29 @@ describe('SocketController', () => {
     socketController.bookController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
     socketController.tourController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
     socketController.tourController.getAllSort = jest.fn(() => Promise.resolve(true));
+    socketController.tourController.createDocs = jest.fn(() => Promise.resolve(true));
     const result = await socketController.routing();
     jest.advanceTimersByTime(2000);
     expect(result).toBe(true);
   });
-  it('runs routing but has error on getting the tours', async () => {
+  it('runs routing with no errors', async () => {
     const socketController = new SocketController(sServer);
     socketController.bookController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
+    socketController.bookController.createDocs = jest.fn(() => Promise.resolve(true));
+    socketController.tourController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
+    socketController.tourController.getAllSort = jest.fn(() => Promise.resolve(true));
+    socketController.tourController.createDocs = jest.fn(() => Promise.resolve(true));
+    const result = await socketController.routing();
+    jest.advanceTimersByTime(2000);
+    expect(result).toBe(true);
+  });
+  it('runs routing but has error on getting and creating the tours', async () => {
+    const socketController = new SocketController(sServer);
+    socketController.bookController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
+    socketController.bookController.createDocs = jest.fn(() => Promise.resolve(true));
     socketController.tourController.deleteAllDocs = jest.fn(() => Promise.resolve(true));
     socketController.tourController.getAllSort = jest.fn(() => Promise.reject(new Error('bad')));
+    socketController.tourController.createDocs = jest.fn(() => Promise.reject(new Error('bad')));
     const result = await socketController.routing();
     jest.advanceTimersByTime(2000);
     expect(result).toBe(true);
